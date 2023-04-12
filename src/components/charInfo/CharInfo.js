3851f2c83d10ce149fc1b67ae3ffd547df87547b
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-04-12 12:54:20 +0300

    -ESLINT comment

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -97,6 +97,7 @@ const View = ({ char }) => {
       <ul className="char__comics-list">
         {comics.length > 0 ? null : "There is no comics with this character"}
         {comics.map((item, i) => {
+          // eslint-disable-next-line array-callback-return
           if (i > 10) return;
           return (
             <li key={i} className="char__comics-item">
