453ded2a529dc1e8c043c7c6d27c91590ef64d73
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-04-19 16:30:26 +0300

    fix visual

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -47,7 +47,7 @@ const View = ({ comic }) => {
       <div className="single-comic__info">
         <h2 className="single-comic__name">{title}</h2>
         <p className="single-comic__descr">{description}</p>
-        <p className="single-comic__descr">{pageCount} pages</p>
+        <p className="single-comic__descr">{pageCount}</p>
         <p className="single-comic__descr">Language: {language}</p>
         <div className="single-comic__price">{price}</div>
       </div>
