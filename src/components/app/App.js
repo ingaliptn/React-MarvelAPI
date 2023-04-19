cfd42c1241e8f5867328ce6528174de4458a3d17
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-04-19 13:36:06 +0300

    Create Comics List

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,10 +2,11 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-
+import AppBanner from "../appBanner/AppBanner";
 import decoration from "../../resources/img/vision.png";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import { useState } from "react";
+import ComicsList from "../comicsList/ComicsList";
 
 const App = () => {
   const [selectedChar, setChar] = useState(null);
@@ -18,7 +19,7 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        <ErrorBoundary>
+        {/* <ErrorBoundary>
           <RandomChar />
         </ErrorBoundary>
         <div className="char__content">
@@ -27,7 +28,9 @@ const App = () => {
             <CharInfo charId={selectedChar} />
           </ErrorBoundary>
         </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
+        <img className="bg-decoration" src={decoration} alt="vision" /> */}
+        <AppBanner />
+        <ComicsList />
       </main>
     </div>
   );
