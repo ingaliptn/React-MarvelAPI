acb083baf715e88da56915cb9d205cd692b1da71
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-05-10 13:25:10 +0300

    Add FindByName

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,11 +10,16 @@ const useMarvelService = () => {
   const _baseOffset = 210;
 
   const getAllCharacters = async (offset = _baseOffset) => {
-    const res = await request(
-      `${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
-    );
-    return res.data.results.map(_transformCharacter);
-  };
+		const res = await request(
+			`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
+		);
+		return res.data.results.map(_transformCharacter);
+	};
+
+	const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
 
   const getCharacter = async (id) => {
     const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
@@ -71,6 +76,7 @@ const useMarvelService = () => {
     getCharacter,
     getAllComics,
     getComics,
+    getCharacterByName
   };
 };
 
